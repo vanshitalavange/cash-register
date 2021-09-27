453bbcd0edcf1715f86337b00541cbaf1bf6b06e
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-09-28 03:26:50 +0530

    js added

---
+++ app.js
@@ -0,0 +1,39 @@
+const billAmount = document.querySelector("#bill-amount");
+const checkButton = document.querySelector("#btn-check");
+const cashGiven = document.querySelector("#cash-given");
+const errorMessage = document.querySelector("#error-message");
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+function showMessage(message) {
+  errorMessage.style.display = "block";
+  errorMessage.textContent = message;
+}
+
+function calculateChange(amountToBeReturned) {
+  for (let i = 0; i < availableNotes.length; i = i + 1) {
+    var numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    console.log(amountToBeReturned[i], noOfNotes);
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    noOfNotes[i].textContent = numberOfNotes;
+  }
+} 
+function hideMessage() {
+  errorMessage.style.display = "none";
+}
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  hideMessage();
+  
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value) {
+          const amountToBeReturned = cashGiven.value - billAmount.value;
+          calculateChange(amountToBeReturned);
+        } else {
+          showMessage(
+            "The cash provided should atleast be equal to the bill amount"
+          );
+        }
+      } else {
+        showMessage("The bill amount should be greater than 0");
+      }
+ });
